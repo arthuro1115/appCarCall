0f1f2d09fa9c8d60df11346b45f6f8c266bcce47
arthuro1115 <58000481+arthuro1115@users.noreply.github.com>
2020-05-13 18:54:56 -0300

    Create ProfileActivity.java

---
+++ ProfileActivity.java
@@ -0,0 +1,46 @@
+package com.example.lg_beta;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        //Coloca Home Como principal
+        bottomNavigationView.setSelectedItemId(R.id.profile);
+        //Selecionar Abas
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                switch (menuItem.getItemId()){
+                    case R.id.profile:
+                        return true;
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext()
+                                , MainActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.localization:
+                        startActivity(new Intent(getApplicationContext()
+                                , LocalizationActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                }
+                return false;
+            }
+        });
+
+    }
+}
